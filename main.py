a0c0db0a60e93e28f67174d6c5a908aef3162167
smathe1308 <52777795+smathe1308@users.noreply.github.com>
2019-07-15 15:08:29 +0530

    Update main.py
    Updating code to fetch customers by Id.

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from google.cloud import datastore
 # Setting up environ variables for connecting datastore on GCP                                #
 ###############################################################################################
 #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'E:\myWork\GCP\\test-project-130887-9cb782281074.json'
-#os.environ['FLASK_ENV'] = 'development'
 
 app = Flask(__name__)
 
@@ -15,56 +14,58 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello, Welcome to customers database : Enter /getCustomers to list all customers or /getCustomer?name=cust_name to get a single customer detail'
+        return 'Hello, Welcome to customers database : Enter /getCustomers to list all customers or /getCustomer?id=101 to get a single customer detail'
 
 	
 ###############################################################################################
 # Get all customers from the datastore.                                                       #
-# http://127.0.0.1:5000/getCustomers will give a json string of all the customers from           #
+# http://127.0.0.1:5000/getCustomers will give a json string of all the customers from        #
 # the datastore.                                                                              #
 ###############################################################################################
 @app.route('/getCustomers')
 def get_customers():
     client = datastore.Client()
-    query = client.query(kind='Customer')
+    query = client.query(kind='customer')
     customers = list(query.fetch())
     return json.dumps(customers)
 
 
 ###############################################################################################
 # Get customer filtered by name                                                               #
-# http://127.0.0.1:5000/getCustomer?name=xyz will show all customers by the name xyz             #
+# http://127.0.0.1:5000/getCustomer?id={id} will show fetch customer by a particular id.      #
 ###############################################################################################
 @app.route('/getCustomer')
-def get_customer_by_name():
-    name = request.args.get('name', None)
+def get_customer_by_id():
+    id = int(request.args.get('id', None))
     client = datastore.Client()
-    query = client.query(kind='Customer')
-    query.add_filter(property_name='name', operator='=',value=name)
+    query = client.query(kind='customer')
+    query.add_filter(property_name='id', operator='=', value=id)
     customer_filter = list(query.fetch())
     result = json.dumps(customer_filter)
     return result
 
 
-###############################################################################################
-# Adding new entry in the datastore                                                           #
-# http://127.0.0.1:5000/add?name=xyz&age=20 will add a new entry with name = xyz and age = 20 #
-###############################################################################################
+##############################################################################################################
+# Adding new entry in the datastore                                                           		     #
+# http://127.0.0.1:5000/add?id={id}&name={name}&age={age} will add a new entry.				     #
+##############################################################################################################
 @app.route('/add')
 def add_customer():
     name = request.args.get('name', None)
     age = request.args.get('age', None)
+    id = int(request.args.get('id', None))
     client = datastore.Client()
-    entity_kind = 'Customer'
+    entity_kind = 'customer'
     key = client.key(entity_kind)
     key = client.allocate_ids(key, 1)[0]
     entity = datastore.Entity(key=key)
     entity.update({
         'name': name,
-        'age': age
+        'age': age,
+        'id': id
     })
     client.put(entity)
-    return "New Entity Created for Name : %s" %name
+    return "New Entity Created for Name : %s" % name
 
 
 if __name__ == '__main__':
